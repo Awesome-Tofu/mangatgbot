0fc6a491217b61863dc9be53b73df397670078d2
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-10-07 23:51:02 +0530

    Update start.py

--- Manga/plugins/start.py
+++ Manga/plugins/start.py
@@ -8,3 +8,11 @@ from Manga import prefix
 async def start(_, message):
     user_id = int(message.from_user.id)
     mention = message.from_user.mention
+
+markup = InlineKeyboardMarkup([
+    [InlineKeyboardButton("OWNER", url="https://t.me/just_a_bio"),
+     InlineKeyboardButton("SUPPORT", url="https://t.me/just_a_bio")]
+])
+
+caption = "Bot Awake 🌟"
+photo = "https://telegra.ph/file/dc59c2b7bdfc7132895f1.jpg"
