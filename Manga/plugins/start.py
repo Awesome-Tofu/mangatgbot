771e6e8e416f6e4a505616827a9ce63ccae92107
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-10-07 23:50:06 +0530

    Update start.py

--- Manga/plugins/start.py
+++ Manga/plugins/start.py
@@ -1,1 +1,10 @@
+import asyncio
+from pyrogram import filters, enums 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from Manga import bot
+from Manga import prefix
 
+@bot.on_message(filters.command("start", prefix) | filters.private)
+async def start(_, message):
+    user_id = int(message.from_user.id)
+    mention = message.from_user.mention
